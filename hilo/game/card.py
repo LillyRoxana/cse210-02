7fb9d5d370185c2e48506b137111c335d93da30f
Jesustortoleroh <tor19021@byui.edu>
2022-05-06 14:31:38 -0400

    Commit new branch

--- hilo/game/card.py
+++ hilo/game/card.py
@@ -22,6 +22,7 @@ import random
 ## - Enhanced game play and game over messages.
 ## - Enhanced game display, e.g. card suits
 
+
 class Card:
     def __init__(self):
         self.score = 0
@@ -32,7 +33,7 @@ class Card:
 
     def play(self):
         self.value = random.randint(1, 13)
-        self.guess = input("Higher or lower? [h/l]: ")## INPUT ##
+        self.guess = input("Higher or lower? [h/l]:  ")## INPUT ##
         self.prior = self.value
         if (self.guess == "h" and self.value > self.prior) or (self.guess == "l" and self.value < self.prior):
             self.score = 100
@@ -40,36 +41,3 @@ class Card:
             self.score = -75
 
         self.total_score += self.score
-
-class Director:
-
-    def __init__(self):
-        self.card = []
-        self.is_playing = True
-        self.score = 300
-        self.total_score = 0
-
-    def start_game(self):
-        print("Welcome to Hilo Card Game")
-        while self.is_playing:
-            self.get_inputs()
-            self.get_guess()            
-            self.do_updates()
-            self.do_outputs()
-    
-    def get_inputs(self):
-        play_card = input("Play again? y/n")
-        self.is_playing = (play_card == "y")
-
-    def get_guess(self):
-        guess_card = input("Higher or lower? h/l")
-        self.is_playing = (guess_card == "h" or guess_card == "l")
-
-    def do_updates(self):
-        if not self.is_playing: 
-            return
-
-    def do_outputs(self):
-        if not self.is_playing: 
-            return
-
