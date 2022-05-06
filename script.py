d2c0c544159140fd1cca467ed9bd3e3e981be308
flixjavier <flixjavier@gmail.com>
2022-05-05 21:04:03 -0600

    I added class Director, made some changes on some methods.

--- script.py
+++ script.py
@@ -1,3 +1,4 @@
+import random
 ## rules
 ## The player starts the game with 300 points.
 ## Individual cards are represented as a number from 1 to 13.
@@ -25,19 +26,28 @@
 class Card:
     def __init__(self):
         self.score = 0
+        #card value
         self.value = 0
-        self.play = true
+        self.total_score = 0
+        self.play = True
 
     def play(self):
         self.value = random.randint(1, 13)
-        self.guess = ## INPUT ##
-        self.prior = 
-        self.score = 100 if self.guess == "h" and self.value > self.prior or self.guess = "l" and self.value < self.prior
+        self.guess = input("Higher or lower? [h/l]: ")## INPUT ##
+        self.prior = self.value
+        self.score = 100 if self.guess == "h" and self.value > self.prior or self.guess == "l" and self.value < self.prior
           else self.score = -75
+        
+        self.total_score += self.score
 
+    def 
 
 # class2 goes here
-
+class Director:
+    def start_game(self):
+        print("Welcome to Hilo Card Game")
+        while self.play == True:
+            self.play()
 # method goes here
 
 print(self.score)
