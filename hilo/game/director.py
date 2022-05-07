f6dca8a5827c167a5a924688e8586c1d53aa6a14
Josh Ausborne <63316430+joshausborne@users.noreply.github.com>
2022-05-07 02:00:18 -0400

    start trying to get the do_updates function to print some things

--- hilo/game/director.py
+++ hilo/game/director.py
@@ -3,30 +3,39 @@ from game.hilo import Hilo
 class Director:
 
     def __init__(self):
-        self.card = []
+        self.hilo = []
         self.is_playing = True
         self.score = 300
         self.total_score = 0
+        self.guess = ""
 
     def start_game(self):
         print("Welcome to Hilo Card Game")
         while self.is_playing:
             self.get_inputs()
-            self.get_guess()            
+            self.get_guess()
             self.do_updates()
             self.do_outputs()
     
     def get_inputs(self):
-        play_card = input("Play again? y/n")
+        play_card = input("Play again? [y/n] ")
         self.is_playing = (play_card == "y")
 
     def get_guess(self):
-        guess_card = input("Higher or lower? h/l")
+        guess_card = input("Higher or lower? [h/l] ")
         self.is_playing = (guess_card == "h" or guess_card == "l")
 
     def do_updates(self):
         if not self.is_playing: 
             return
+
+        values = ""
+        if self.guess == "h":
+            print(f"You guessed that the card would be higher.")
+        elif self.guess == "l":
+            print(f"You guess that the card would be lower.")
+        else:
+            print(f"You done messed up.")
 
     def do_outputs(self):
         if not self.is_playing: 
