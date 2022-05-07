f8e819573f985392070c370258da0ef2f889b87d
Jesustortoleroh <tor19021@byui.edu>
2022-05-07 15:37:40 -0400

    Updated With Comments

--- hilo/game/card.py
+++ hilo/game/card.py
@@ -1,10 +1,23 @@
 import random
 
 class Card:
+    """The responsability of Card is drawing the cards, keeps track
+    of the previous card, and determines if the drawn card is hi or lo.
+    Attributes:
+        
+        guess: a instant of Card
+        current_card: a random card drawn from the deck
+        next_card: the previous card, used to evaluate hi/lo
+    """
+   
    
 
     def __init__(self):
         
+        """Constructs a new instance of Card.
+        Args:
+            self (Card): An instance of Card.
+        """
         self.current_card = []
         self.next_card = []
         self.guess = []
@@ -12,7 +25,11 @@ class Card:
        
         
     def deal_card(self):
+        """Generates a new random value and calculates the result for the Card.
         
+        Args:
+            self (Card): An instance of Card.
+        """
         self.current_card.clear()
         result = random.randint(1,13)
         self.current_card.append(result)
@@ -26,7 +43,11 @@ class Card:
     
     
     def deal_next_card(self):
+        """Generates a new random value and calculates the result for the next Card.
         
+        Args:
+            self (Card): An instance of Card.
+        """
         self.next_card.clear()
         next_result = random.randint(1,13)
         self.next_card.append(next_result)
@@ -34,7 +55,16 @@ class Card:
     
     
     def get_points(self):
-       
+        
+        """ Determines if the drawn (current) card is higher or lower than
+        the previous card.
+        Args:
+            self (Card): An instance of the Card class.
+        Returns:
+            str: if higher, returns 'hi'
+                 if lower, returns 'lo'
+                 if equal, returns 'equal'
+        """
         guess = self.guess
         score = 0
         if guess == "h":
--- hilo/game/director.py
+++ hilo/game/director.py
@@ -1,9 +1,24 @@
 from game.card import Card
+  
 
 class Director:
+          
+    """A person who directs the game. 
+    
+    The responsibility of a Director is to control the sequence of play.
+    Attributes:
+        is_playing (boolean): Whether or not the game is being played.
+        score (int): The score for one round of play.
+        dealer (int): The score for the entire game.
+    """
    
     def __init__(self):
+      
+        """Constructs a new Director.
         
+        Args:
+            self (Director): an instance of Director.
+        """
         self.keep_playing = True
         self.score = 300
         self.dealer = Card()
@@ -11,8 +26,12 @@ class Director:
     
     def start_game(self):
         
+        """Starts the game by running the main game loop.
+        
+        Args:
+            self (Director): an instance of Director.
+        """
        
-
         while self.keep_playing:
             
             self.get_inputs()
@@ -22,6 +41,11 @@ class Director:
 
     
     def get_inputs(self):
+        
+        """Ask the user if they want to roll.
+        Args:
+            self (Director): An instance of Director.
+        """
         print(f"Welcome to Hilo Game!")
         self.dealer.deal_card()
         print(f"\nThe card is: {self.dealer.current_card}")
@@ -33,7 +57,11 @@ class Director:
 
 
     def do_updates(self):
-    
+        
+        """Updates the player's score.
+        Args:
+            self (Director): An instance of Director.
+        """
         points = self.dealer.get_points()
         self.score += points
        
@@ -41,7 +69,10 @@ class Director:
          
    
     def do_outputs(self):
-      
+        """Displays the Hilo and the score. Also asks the player if they want to roll again. 
+        Args:
+            self (Director): An instance of Director.
+        """
         print(f"Your score is: {self.score}")
         if self.can_deal():
             choice = input("Keep playing? [y/n]: ")
@@ -50,12 +81,7 @@ class Director:
             self.keep_playing = False
     
     
-    
-    
-    
-    
-   
+  
     def can_deal(self):
         
         return self.score > 0
-
