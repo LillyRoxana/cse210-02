a1a71a4688d2eb0494a86b019c88df9fc600a0ce
Josh Ausborne <63316430+joshausborne@users.noreply.github.com>
2022-05-07 22:28:14 -0400

    move rules and reqs from director.py to README

--- director.py
+++ director.py
@@ -1,52 +1,17 @@
 from shuffle import Hilo
 
-## Rules
-## - The player starts the game with 300 points.
-## - Individual cards are represented as a number from 1 to 13.
-## - The current card is displayed.
-## - The player guesses if the next one will be higher or lower.
-## - The the next card is displayed.
-## - The player earns 100 points if they guessed correctly.
-## - The player loses 75 points if they guessed incorrectly.
-## - If a player reaches 0 points the game is over.
-## - If a player has more than 0 points they decide if they want to keep playing.
-## - If a player decides not to play again the game is over.
-## 
-## Requirements
-## - The program must include a README file.
-## - The program must include class and method comments.
-## - The program must have at least two classes.
-## - The program must remain true to game play described in the overview.
-## 
-## Things to do for fun:
-## - Enhanced input validation.
-## - Enhanced game play and game over messages.
-## - Enhanced game display, e.g. card suits
-
 class Director:
-    """A person who directs the game. 
-        
-        The responsibility of a Director is to control the sequence of play.
 
-        Attributes:
-            card (Hilo): A number between 1 to 13.
-            is_playing (boolean): Whether or not the game is being played.
-            score (int): The score for one round of play.
-            total_score (int): The score for the entire game.
-        """
     def __init__(self):
-        """Constructs a new Director.
-        
-        Args:
-            self (Director): an instance of Director.
-        """
         self.card = Hilo()
         self.is_playing = True
         self.score = 300
         self.total_score = 0
 
-#*This method start the game. Have a while loop that help us to continue the game, 
-#*until the player reach 0 or less, or until the player inputs "n"
+# Method:
+# Start the game here. Use while loope to help the game continue. Game will until
+# player reaches 0 points, or until player selects "n" at the prompt.
+
     def start_game(self):
         print("Welcome to Hilo Card Game")
         while self.is_playing:
@@ -55,7 +20,11 @@ class Director:
             self.get_inputs()  
             self.update()       
 
-#*Method where the player need to guess if the card is higher or lower. If he choose right gets 100 points, if not -75 points. 
+
+# Method:
+# The player needs to get if the next card will be higher or lower. If they choose
+# correctly, they get 100 points. If they choose incorrectly, they lose 75 points.
+
     def get_inputs(self):
         prior = self.card2
         choice = input("Higher or lower? [h/l] ")
@@ -77,7 +46,10 @@ class Director:
             print("That was not a valid option")
             self.get_inputs()
 
-#*Method to check the total score. And where the player decide if he wants to continue. 
+# Method:
+# Check to see if the score is less than or equal to zero. Also inquire if the
+# player wants to continue playing.
+
     def update(self):
         if self.total_score <= 0:
             print(f"Your score is: {self.total_score}")
